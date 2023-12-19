10fcc67c884bc2d8ce73131085f0bfc805b84d4a
Ryan Kline <ryanjkline@protonmail.com>
2023-12-19 15:13:15 -0500

    Adds vertical and horizontal splitting

--- lua/ryan/remap.lua
+++ lua/ryan/remap.lua
@@ -1,3 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Vertical split
+vim.api.nvim_set_keymap('n', '<leader>wv', ':vsplit<CR>', { noremap = true, silent = true })
+
+-- Horizontal split
+vim.api.nvim_set_keymap('n', '<leader>wh', ':split<CR>', { noremap = true, silent = true })
+
+-- Close current window
+vim.api.nvim_set_keymap('n', '<leader>wq', ':close<CR>', { noremap = true, silent = true })
+
+-- Jump to the next window
+vim.api.nvim_set_keymap('n', '<leader>ww', '<C-w>w', { noremap = true, silent = true })
+
+-- Jump to the previous open file
+vim.api.nvim_set_keymap('n', '<leader>wp', ':bprev<CR>', { noremap = true, silent = true })
+
+

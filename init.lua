7abc7fb041d53838654e87469d3e97bbe8ad3d5d
Ryan Kline <ryanjkline@protonmail.com>
2023-12-18 20:33:58 -0500

    Adds more plugins

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ local plugins = {
 	{'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{'tpope/vim-fugitive'},
 }
 
 local options = {}
